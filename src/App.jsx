e63890e0d76f94a60a4f0fbc394980b7cdb9e827
ArianBenitez14 <arianbe14@gmail.com>
2024-11-12 21:57:18 -0300

    Commit
    Colaboradores: Antonella, Arian, Mathias

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import MovieList from './components/MovieList';
-import moviesData from './data/movies.json';
+import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    setMovies(moviesData);
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(
+          'https://api.themoviedb.org/3/discover/movie?api_key=8bf671b37acf42e92c1ce6bc24023aa6&include_adult=false&page=1&sort_by=popularity.desc&vote_count.gte=40'
+        );
+        const data = await response.json();
+        setMovies(data.results);
+      } catch (error) {
+        console.error('no estan las pelis : ', error);
+      }
+    };
+    fetchMovies();
   }, []);
 
   return (
-    <div className=" d-flex flex-column justify-content-center align-items-center">
-      <header>
-        <h1 className="text-center">Hack flix</h1>
+    <div className="d-flex flex-column justify-content-center align-items-center">
+      <header className="banner">
+        <h1 className="">Hackflix</h1>
       </header>
       <div className="m-5">
         <MovieList movies={movies} />
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import './Card.css';
 
 const MovieCard = ({ movie }) => {
+  const imgUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
   return (
-    <div className="movie-card card  hover-effect">
-      <img
-        className="card-img-top "
-        src={movie.poster_path}
-        alt={movie.title}
-      />
+    <div className="movie-card card hover-effect">
+      <img className="card-img-top" src={imgUrl} alt={movie.title} />
     </div>
   );
 };
