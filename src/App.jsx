a5888503fb4a2c524531471749ca254fa13bf604
ArianBenitez14 <arianbe14@gmail.com>
2024-11-15 21:31:36 -0300

    Commit
    Colaboradores: Antonella, Mathias, Arian

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,58 @@
-import React, { useState, useEffect } from "react";
-import MovieList from "./components/MovieList";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import MovieList from './components/MovieList';
+import './App.css';
+import { Rating } from 'react-simple-star-rating';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [selectedGenre, setSelectedGenre] = useState("");
+  const [selectedGenre, setSelectedGenre] = useState('');
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [rating, setRating] = useState(0);
+  const [page, setPage] = useState(1);
 
-  /*useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await fetch(
-          "https://api.themoviedb.org/3/discover/movie?api_key=8bf671b37acf42e92c1ce6bc24023aa6&include_adult=false&page=1&sort_by=popularity.desc&vote_count.gte=40"
-        );
-        const data = await response.json();
-        setMovies(data.results);
-      } catch (error) {
-        console.error("no estan las pelis : ", error);
-      }
-    };
-    fetchMovies();
-  }, []);*/
+  const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=8bf671b37acf42e92c1ce6bc24023aa6&include_adult=false&sort_by=popularity.desc&vote_count.gte=40`;
 
-  const fetchMovies = async (genre = "") => {
+  const fetchMovies = async (genre = '') => {
     try {
-      let url = `https://api.themoviedb.org/3/discover/movie?api_key=8bf671b37acf42e92c1ce6bc24023aa6&include_adult=false&page=1&sort_by=popularity.desc&vote_count.gte=40`;
-
-      if (genre) {
-        url += `&with_genres=${genre}`;
-      }
-
-      const response = await fetch(url);
+      const response = await fetch(`${API_URL}&page=${page}`);
       const data = await response.json();
       setMovies(data.results);
+      setFilteredMovies(data.results);
     } catch (error) {
-      console.error("No se encontraron las películas: ", error);
+      console.error('No se encontraron las películas: ', error);
     }
   };
 
   useEffect(() => {
     fetchMovies();
-  }, []);
+  }, [page]);
+
+  const applyFilters = () => {
+    let filtered = movies;
+
+    if (selectedGenre) {
+      filtered = filtered.filter((movie) =>
+        movie.genre_ids.includes(selectedGenre)
+      );
+    }
+
+    if (rating > 0) {
+      const minRating = rating / 2;
+      filtered = filtered.filter((movie) => movie.vote_average >= minRating);
+    }
+    setFilteredMovies(filtered);
+  };
 
   const handleGenreClick = (genreId) => {
     setSelectedGenre(genreId);
-    fetchMovies(genreId);
   };
+  const handleRating = (rate) => {
+    setRating(rate);
+  };
+
+  useEffect(() => {
+    applyFilters();
+  }, [selectedGenre, rating, movies]);
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
@@ -90,11 +98,28 @@ function App() {
           </div>
         </nav>
       </header>
+
       <div className="banner">
         <h1 className="title-size">Hackflix</h1>
       </div>
-      <div className="m-5 cards-div">
-        <MovieList movies={movies} />
+
+      <div className="m-5">
+        <div className="mb-4">
+          <Rating
+            onClick={handleRating}
+            size={30}
+            transition
+            allowHalfIcon
+            initialValue={0}
+          />
+        </div>
+        {filteredMovies.length > 0 ? (
+          <MovieList movies={filteredMovies} page={setPage} />
+        ) : (
+          <p className="text-center">
+            Lo sentimos, no se encontraron películas con los filtros aplicados.
+          </p>
+        )}
       </div>
     </div>
   );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,24 @@
-import React from "react";
-import MovieCard from "./MovieCard";
+import React from 'react';
+import MovieCard from './MovieCard';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const MovieList = ({ movies }) => {
   return (
-    <div className="row no-gutters">
-      {movies.map((movie) => (
-        <div className="col-3  mb-4" key={movie.id}>
-          <MovieCard movie={movie} />
-        </div>
-      ))}
-    </div>
+    <>
+      <InfiniteScroll dataLength={movies.length} next={() => setPage + 1}>
+        {
+          <div className="row no-gutters">
+            {movies.map((movie) => (
+              <div className="col-3  mb-4" key={movie.id}>
+                <MovieCard movie={movie} />
+              </div>
+            ))}
+          </div>
+        }
+      </InfiniteScroll>
+    </>
   );
 };
 
 export default MovieList;
+s;
